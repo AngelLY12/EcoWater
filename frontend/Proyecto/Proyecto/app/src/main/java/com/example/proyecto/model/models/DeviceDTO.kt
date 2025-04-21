1fb4c4d765f7b468ee01726fe2e9e2efc3974292
Angel <lopezyanezangell@gmail.com>
2025-04-20 20:31:31 -0600

    refactor(models): removed obsolete models after folder restructuring

--- frontend/Proyecto/Proyecto/app/src/main/java/com/example/proyecto/model/models/DeviceDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.example.proyecto.model.models
-
-data class DeviceDTO(
-    val deviceId: String,
-    val user: UserDTO,
-    val tank: TankDTO
-)
--- frontend/Proyecto/Proyecto/app/src/main/java/com/example/proyecto/model/models/DeviceType.kt
+++
@@ -1,5 +0,0 @@
-package com.example.proyecto.model.models
-
-enum class DeviceType {
-        SENSOR_PROXIMIDAD, CAUDALIMETRO
-}

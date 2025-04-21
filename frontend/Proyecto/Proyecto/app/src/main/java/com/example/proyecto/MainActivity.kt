433b8aeb36b7666dd978de87e4121154a438768c
Angel <lopezyanezangell@gmail.com>
2025-04-20 20:36:51 -0600

    chore: minor updates in MainActivity

--- frontend/Proyecto/Proyecto/app/src/main/java/com/example/proyecto/MainActivity.kt
+++ frontend/Proyecto/Proyecto/app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -4,43 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.SnackbarHostState
-import androidx.compose.material.Surface
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.login.AppNavigation
-import com.example.login.LoginScreen
-import com.example.login.RegisterScreen
-import com.example.proyecto.ui.components.AppScaffold
-import com.example.proyecto.ui.components.CustomToast
+import com.example.proyecto.ui.components.navigation.AppNavigation
+import com.example.proyecto.ui.components.indicators.AppScaffold
+import com.example.proyecto.ui.components.custom.CustomToast
 import com.example.proyecto.ui.viewModels.AuthViewModel
 import com.example.proyecto.ui.viewModels.ToastViewModel
-import com.example.proyecto.utils.GlobalEvent
-import com.example.proyecto.utils.LogoutHandler
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.launch
+import com.example.proyecto.ui.components.layout.LogoutHandler
 
 
 class MainActivity : ComponentActivity() {

ebe5eb9aaddc31d6c99a669feef3342ee6a70fd5
Angel <lopezyanezangell@gmail.com>
2025-05-05 20:33:25 -0600

    fix: Fixed minor issues with notifications

--- backend/ecoWater/ecoWater/src/main/java/com/project/ecoWater/notification/NotificacionCleanupTask.java
+++ backend/ecoWater/ecoWater/src/main/java/com/project/ecoWater/notification/NotificacionCleanupTask.java
@@ -1,4 +1,22 @@
 package com.project.ecoWater.notification;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
 public class NotificacionCleanupTask {
+    private final NotificationRepository notificationRepository;
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
+    public void cleanupOldNotifications() {
+        LocalDateTime fechaLimite = LocalDateTime.now().minusMonths(1);
+        notificationRepository.deleteByCreatedAtBefore(fechaLimite);
+        System.out.println("Notificaciones antiguas eliminadas");
+    }
 }
